fad6ea8cbc9bd4c3fe95d906e6a046608d879123
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-12-03 21:12:01 +0100

    add sub-modules

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,19 +1,3 @@
-from .rrl import RRL
-
-from .forward import (
-    linear_transform, 
-    activate, 
-    get_positions,
-)
-
-from .backpropagation import (
-    calc_portfolio_returns_derivatives, 
-    calc_positions_derivative, 
-    calc_sharpe_derivative,
-)
-
-from .metrics import (
-    calc_portfolio_returns, 
-    calc_cumulative_profits, 
-    calc_sharpe_ratio, 
-)
+import lib.model
+import lib.metrics
+import lib.data
