998666ac4e36b0f5d723806f24e12a2673367d4d
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-12-26 01:17:20 +0100

    modify Data methods

--- lib/data/dataset.py
+++ lib/data/dataset.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 from dataclasses import dataclass, field
 from pandas.core.frame import DataFrame
-from typing import Tuple
+from typing import Tuple, Optional
 
 from .preprocess import (
     extract_columns, 
@@ -16,6 +16,8 @@ from .preprocess import (
 
 from .indicators import add_indicators
 
+from .pca import get_reduced_features
+
 @dataclass
 class Data:
     """Description. 
@@ -24,26 +26,34 @@ class Data:
     Attributes: 
         - start_date: training/trading start date
         - end_date: training/trading end date
+        - window_size: length of training/trading windows
         - assets: list of asset names 
-        - indicators: list of technical indicators
-        - batch_size: length of training/trading windows
+        - indicators: technical indicators to compute
+        - pca_ncp: optional number of principal components for PCA
     """ 
     start_date: str 
     end_date: str 
-    assets: list=field(default_factory=list) 
-    indicators: list=field(default_factory=list) 
-    window_size:int = 100
+    window_size:int
 
+    assets: list=field(default_factory=list) 
+    indicators: list=field(default_factory=list)
+
+    pca_ncp: Optional[int]=None
 
     def __post_init__(self): 
         """Description. Apply data preprocessing based on class inputs."""
-        
-        self.n_assets, self.n_features = len(self.assets), len(self.indicators)
+
+        self.n_assets = len(self.assets)
+
+        self.n_features = len(self.indicators)
+        if "HTS" in self.indicators: 
+            self.n_features += 1            
 
         self.df = yf.download(self.assets, start=self.start_date, end=self.end_date)
         self.df = extract_columns(self.df, self.assets, ["Close", "Volume", "High", "Low"])
 
         self.batches = to_batches(self.df, self.window_size)
+
 
     def preprocess_batch(self, batch: DataFrame) -> DataFrame: 
         """Description. Compute returns and add indicators for a given batch.
@@ -66,6 +76,10 @@ class Data:
         """Description. Extract feature matrix and returns from batch dataframe."""
 
         features = get_feature_matrix(batch, self.n_assets, self.n_features)
+        
+        if self.pca_ncp != None: 
+            features = get_reduced_features(features, self.pca_ncp)
+
         returns = get_returns_matrix(batch)
 
         return features, returns
